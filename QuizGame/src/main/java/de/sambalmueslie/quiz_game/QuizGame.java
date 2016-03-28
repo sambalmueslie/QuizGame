5bc785544be3f139849d43cd9504e7d5cc18bd50
Oliver Eckle <me@sambalmueslie.de>
2016-03-28 14:25:26 +0200

    add start screen

--- QuizGame/src/main/java/de/sambalmueslie/quiz_game/QuizGame.java
+++ QuizGame/src/main/java/de/sambalmueslie/quiz_game/QuizGame.java
@@ -1,5 +1,10 @@
 package de.sambalmueslie.quiz_game;
 
+import java.io.IOException;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import de.sambalmueslie.quiz_game.config.ConfigFileReader;
 import de.sambalmueslie.quiz_game.data.Model;
 import javafx.application.Application;
@@ -9,6 +14,9 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class QuizGame extends Application {
+
+	/** the {@link Logger}. */
+	private static Logger logger = LogManager.getLogger(QuizGame.class);
 
 	/**
 	 * Main.
@@ -22,21 +30,52 @@ public class QuizGame extends Application {
 
 	@Override
 	public void start(final Stage stage) throws Exception {
-		// ConfigFileReader.createDefaultConfigFile("config.txt");
-		final Model model = ConfigFileReader.readConfigFile("config.txt");
-
-		final FXMLLoader fxmlLoader = new FXMLLoader(getClass().getClassLoader().getResource("MainWindow.fxml"));
-		final Parent root = fxmlLoader.load();
-		final MainWindowController controller = (MainWindowController) fxmlLoader.getController();
-		controller.setModel(model);
-
-		final Scene scene = new Scene(root, 1024, 768);
-		scene.getStylesheets().addAll(getClass().getClassLoader().getResource("style.css").toExternalForm());
-		scene.setOnKeyTyped(e -> controller.handleKeyTyped(e));
-
-		stage.setTitle("FXML Welcome");
-		stage.setScene(scene);
-		stage.show();
+		this.stage = stage;
+		showStartWindow();
 	}
 
+	/**
+	 * Show the main window.
+	 */
+	private void showMainWindow() {
+		try {
+			final Model model = ConfigFileReader.readConfigFile("config.txt");
+			final FXMLLoader fxmlLoader = new FXMLLoader(getClass().getClassLoader().getResource("MainWindow.fxml"));
+			final Parent root = fxmlLoader.load();
+			final MainWindowController controller = (MainWindowController) fxmlLoader.getController();
+			controller.setModel(model);
+
+			final Scene scene = new Scene(root, 1024, 768);
+			scene.getStylesheets().addAll(getClass().getClassLoader().getResource("style.css").toExternalForm());
+			scene.setOnKeyTyped(e -> controller.handleKeyTyped(e));
+
+			stage.setTitle("Quiz Game - Game");
+			stage.setScene(scene);
+			stage.show();
+		} catch (final Exception e) {
+			logger.fatal("Exception occured", e);
+		}
+	}
+
+	/**
+	 * Show the start window.
+	 */
+	private void showStartWindow() throws IOException {
+		try {
+			final FXMLLoader fxmlLoader = new FXMLLoader(getClass().getClassLoader().getResource("StartWindow.fxml"));
+			final Parent root = fxmlLoader.load();
+			final Scene scene = new Scene(root, 1024, 768);
+			scene.getStylesheets().addAll(getClass().getClassLoader().getResource("style.css").toExternalForm());
+			scene.setOnKeyTyped(e -> showMainWindow());
+
+			stage.setTitle("Quiz Game - Welcome");
+			stage.setScene(scene);
+			stage.show();
+		} catch (final Exception e) {
+			logger.fatal("Exception occured", e);
+		}
+	}
+
+	/** the {@link Stage}. */
+	private Stage stage;
 }

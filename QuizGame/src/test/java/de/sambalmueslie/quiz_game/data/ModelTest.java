e2fb1b10f7e9aea7acb00a459d469282daaea7d7
Oliver Eckle <me@sambalmueslie.de>
2016-03-28 10:54:32 +0200

    add test dependencies

---
+++ QuizGame/src/test/java/de/sambalmueslie/quiz_game/data/ModelTest.java
@@ -0,0 +1,24 @@
+package de.sambalmueslie.quiz_game.data;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ModelTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
